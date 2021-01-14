0d027c117b05421f54d11a2e46e375ce59cc91ed
dtracz <dawid.b.tracz@gmail.com>
2021-01-14 07:04:46 +0100

    move basics

--- contracts/Gomoku.sol
+++ contracts/Gomoku.sol
@@ -1,7 +1,32 @@
 pragma solidity ^0.5.0;
+pragma experimental ABIEncoderV2; // for passing structs
 
 contract Gomoku {
-    uint public dummy = 0;
+    Move lastMove;
 
+    struct MoveCode {
+        uint8 x;
+        uint8 y;
+    }
+
+    struct Move {
+        address gameAdsress;
+        uint32 mvIdx;
+        MoveCode moveCode;
+        bytes32 hashPrev;
+        bytes32 hashGameState;
+    }
+
+    function hashMove(Move memory move) pure private returns(bytes32) {
+        // ???
+        return move.hashPrev;
+    }
+
+    function play(Move memory move, bytes32 _signature) public {
+        bytes32 lastHash = hashMove(lastMove);
+        if (lastHash == move.hashPrev) {
+            lastMove = move;
+        }
+    }
 }
 
