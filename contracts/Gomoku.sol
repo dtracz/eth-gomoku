ecd2a62d1e8b11bdc55814da14b81dc2af6c5f30
dtracz <dawid.b.tracz@gmail.com>
2021-01-16 05:36:43 +0100

    reorganize backend

--- contracts/Gomoku.sol
+++ contracts/Gomoku.sol
@@ -1,48 +1,11 @@
 pragma solidity ^0.5.0;
 pragma experimental ABIEncoderV2; // for passing structs
 
-contract Gomoku {
-    address player0;
-    address player1;
 
-    Move lastMove;
-
-    constructor(address _player0, address _player1) public {
-        player0 = _player0;
-        player1 = _player1;
-    }
-
-    modifier playerOnly(uint32 _n) {
-        if (_n % 2 == 0)
-            require(msg.sender == player0);
-        else
-            require(msg.sender == player1);
-        _;
-    }
-
+contract GomokuBackend {
     struct MoveCode {
         uint8 x;
         uint8 y;
-    }
-
-    struct Move {
-        address gameAddress;
-        uint32 mvIdx;
-        MoveCode moveCode;
-        bytes32 hashPrev;
-        bytes32 hashGameState;
-    }
-
-    function hashMove(Move memory move) pure private returns (bytes32) {
-        // ???
-        return move.hashPrev;
-    }
-
-    function play(Move memory move, bytes32 _signature) public playerOnly(move.mvIdx) {
-        bytes32 lastHash = hashMove(lastMove);
-        if (lastHash == move.hashPrev) {
-            lastMove = move;
-        }
     }
 
     struct GameState {
@@ -53,16 +16,29 @@ contract Gomoku {
     GameState gameState;
     int8 winning;
 
-    function placePiece(MoveCode memory _moveCode, int8 _player) private {
+    modifier isMoveCorrect(MoveCode memory _moveCode, int8 _player) {
         require(gameState.board[_moveCode.x][_moveCode.y] == 0);
         require(winning == 0);
+        _;
+    }
+
+    function placePiece(MoveCode memory _moveCode, int8 _player)
+        internal
+        isMoveCorrect(_moveCode, _player)
+        returns(int8)
+    {
         gameState.board[_moveCode.x][_moveCode.y] = _player;
         gameState.nMoves++;
         if (checkWin(_moveCode, _player))
             winning = _player;
+        return _player;
     } 
 
-    function checkWin(MoveCode memory _moveCode, int8 _player) private view returns(bool) {
+    function checkWin(MoveCode memory _moveCode, int8 _player)
+        private
+        view
+        returns(bool)
+    {
         uint8[3][3] memory _sameInDir;
         for (int8 i = -1; i <= 1; i++) {
             for (int8 j = -1; j <= 1; j++) {
@@ -84,6 +60,47 @@ contract Gomoku {
             || _sameInDir[0][0] + _sameInDir[2][2] == 5  // SW-NE diag
             || _sameInDir[2][0] + _sameInDir[0][2] == 5; // NW-SE diag
     }
+}
 
+
+contract Gomoku {
+    address player0;
+    address player1;
+
+    GomokuBackend backend;
+    Move lastMove;
+
+    constructor(address _player0, address _player1) public {
+        player0 = _player0;
+        player1 = _player1;
+    }
+
+    modifier playerOnly(uint32 _n) {
+        if (_n % 2 == 0)
+            require(msg.sender == player0);
+        else
+            require(msg.sender == player1);
+        _;
+    }
+
+    struct Move {
+        address gameAddress;
+        uint32 mvIdx;
+        GomokuBackend.MoveCode moveCode;
+        bytes32 hashPrev;
+        bytes32 hashGameState;
+    }
+
+    function hashMove(Move memory move) pure private returns (bytes32) {
+        // ???
+        return move.hashPrev;
+    }
+
+    function play(Move memory move, bytes32 _signature) public playerOnly(move.mvIdx) {
+        bytes32 lastHash = hashMove(lastMove);
+        if (lastHash == move.hashPrev) {
+            lastMove = move;
+        }
+    }
 }
 
