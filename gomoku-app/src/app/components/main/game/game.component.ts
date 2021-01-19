c5c1c04a33f08f2570eed93ca91378ee0368efb2
Szymon Żak <szymon436@gmail.com>
2021-01-19 02:40:06 +0100

    Added game implementation: phase 1

--- gomoku-app/src/app/components/main/game/game.component.ts
+++ gomoku-app/src/app/components/main/game/game.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {FieldState, GameEthereumService} from "../../../services/game.ethereum.service";
+
+const GOMOKU_SIZE = 19;
 
 @Component({
   selector: 'app-game',
@@ -7,10 +10,31 @@ import {Component, OnInit} from '@angular/core';
 })
 export class GameComponent implements OnInit {
 
-  constructor() {
+  readonly fieldStates: FieldState[][];
+  readonly playerName: string;
+  readonly playerColour: FieldState;
+  readonly gameAddress: string;
+
+  constructor(private gameService: GameEthereumService) {
+    this.playerName = gameService.playerName;
+    this.playerColour = gameService.playerColour;
+    this.gameAddress = gameService.gameAddress;
+    this.fieldStates = new Array(GOMOKU_SIZE);
+    for (let i = 0; i < GOMOKU_SIZE; i++) {
+      this.fieldStates[i] = new Array(GOMOKU_SIZE).fill(0);
+    }
   }
 
   ngOnInit(): void {
   }
 
+  setColour(i, j) {
+    this.fieldStates[i][j] = this.gameService.playerColour;
+    console.log(`Index[${i}][${j}] update`)
+    console.log(this.fieldStates);
+  }
+
+  sendMove(i, j) {
+    this.gameService.sendMove(i, j);
+  }
 }
