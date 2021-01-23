9b568514995c85873f36c661b6a3d42d9003d72c
Czarska, Gabriela <gczarska@qualtrics.com>
2021-01-23 02:13:05 +0100

    add move service

--- gomoku-app/src/app/services/move.service.ts
+++ gomoku-app/src/app/services/move.service.ts
@@ -7,6 +7,7 @@ declare let require: any;
 const Web3 = require('web3');
 const contractPath = require('../../../../build/contracts/Gomoku.json');
 const contract = require("@truffle/contract");
+const Web3EthAbi = require('web3-eth-abi');
 
 @Injectable({
   providedIn: 'root'
@@ -17,15 +18,51 @@ export class MoveService extends AbstractContractService {
     super();
   }
 
+  MoveType = {
+    'Move' : {
+      'gameAddress': 'address',
+      'mvIdx': 'uint32',
+      'code': 'string',
+      'hashPrev': 'bytes32',
+      'hashGameState': 'bytes32'
+    }
+  }
+
   move(playerName: string, gameAddress: string) {
     this.getAccount();
     const gomokuContract = contract(contractPath);
+
+    var zero32 = '0x0000000000000000000000000000000000000000000000000000000000000000'
+    const move = {
+      'gameAddress': gameAddress,
+      'mvIdx': 1,
+      'code': "(10,10)",
+      'hashPrev': zero32,
+      'hashGameState': zero32
+    }
+
+    const signature = this.sign(move, this.MoveType, this.account)
     gomokuContract.setProvider(this.web3);
-    // gomokuContract.deployed().then(instance => {
-    //   instance.joinGame(playerName, gameAddress,
-    //     {
-    //       from: this.account
-    //     });
-    // });
+    gomokuContract.deployed().then(instance => {
+      instance.play(move, signature,
+        {
+          from: this.account
+        });
+    });
+  }
+
+  sign = (struct, structType, account) => {
+    const code = Web3EthAbi.encodeParameter(structType, struct)
+    let signature = window.web3.eth.sign(account, code);
+    signature = signature.substr(2);
+    const _r = '0x' + signature.slice(0, 64);
+    const _s = '0x' + signature.slice(64, 128);
+    const _v = '0x' + signature.slice(128, 130);
+    let v_decimal = window.web3.toDecimal(_v);
+    if(v_decimal != 27 || v_decimal != 28) {
+      v_decimal += 27
+    }
+    signature = {v: v_decimal, r: _r, s: _s};
+    return signature
   }
 }
